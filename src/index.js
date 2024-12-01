d6cce989e8f09de5fc17e5a1824dd12aa835472f
dhimal-bhupraj <bhuprajdhimal@gmail.com>
2024-12-01 13:36:20 +0545

    sample commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider
+    domain="dev-li1c5n12f7webcnc.us.auth0.com"
+    clientId="jlaDp4CRp01P6gHZBrJ1lIQNpjTbYi6O"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}>
     <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
